c7192f29b672202890ed3cee053a803ee2dfe579
basillatif <basillatif@gmail.com>
2019-04-13 17:12:55 -0700

    First Var Dec File

---
+++ ScalaVarDec.scala
@@ -0,0 +1,11 @@
+object Demo {
+   def main(args: Array[String]) {
+      var twoDigit :Int = 10;
+      val greeting :String = "Hello Scala with datatype declaration.";
+      var twoTimes10 = 20;
+      val greetNoDec = "Hello Scala new without datatype declaration.";
+
+      println(twoDigit); println(greeting); println(twoTimes10); 
+      println(greetNoDec);
+}
+}
